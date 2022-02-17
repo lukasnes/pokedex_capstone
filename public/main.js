1ec211752ae0508a78dcc664e8cfa5770ec40750
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-02-17 14:03:30 -0600

    SidebarFunctional

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector('[data-menu-btn]')
+const sidebar = document.querySelector('[data-sidebar]')
 
 menuBtn.addEventListener('click', () => {
-    
+    sidebar.classList.toggle('open')
 })
